efc8dfff49aa044dcb6939b926f8ae9ca5db4ee8
Jonathan Perry <jonathanperry@modernpoacher.com>
2023-05-02 18:39:59 +0100

    Removes Gulp

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,11 +1,9 @@
 declare module '@modernpoacher/rules-runner' {
-  class RulesRunner {
+  export class RulesRunner {
     constructor (config?: object)
     run (values?: object, decisions?: object): object
     runTests (expectations?: object, values?: object): object
     runTest (expectation?: object): object
     runOutcomes (outcomes?: object, accumulator?: object): object
   }
-
-  export = RulesRunner
 }
